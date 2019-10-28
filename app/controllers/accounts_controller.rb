1888e076ddfda14ee4ee8f921702bd5b6a90c1f8
knguye69 <knguye69@uncc.edu>
2019-10-28 16:24:48 +0000

    Add link from welcome page to user page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,19 @@
 class AccountsController < ApplicationController
     def new
+        
     end
+    
     def create
-        render plain: params[:account].inspect
+        @account = Account.new(account_params)
+        
+        @account.save
+        render 'new'
     end 
 end
+
+ private def account_params
+        params.require(:account).permit(:fName,:lName,:email,:userName,:password,:age)
+    end   
+
+
+
