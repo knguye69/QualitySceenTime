507b176b11d932eee6ff151a217d3a204a8d8f1e
knguye69 <knguye69@uncc.edu>
2019-12-05 23:04:43 +0000

    Update migrate

--- db/migrate/20191205171530_fix_column_name.rb
+++ db/migrate/20191205171530_fix_column_name.rb
@@ -1,6 +1,6 @@
 class FixColumnName < ActiveRecord::Migration[5.2]
   def change
-    rename_column :accounts, :fName, :goal
+    
     rename_column :accounts, :lName, :time
     rename_column :accounts, :email, :motivation
     rename_column :accounts, :age, :plan
